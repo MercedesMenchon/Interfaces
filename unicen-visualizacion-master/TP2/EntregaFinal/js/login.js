7f0a079751c0b3fa4ddd996bb98dc2ab0b176716
Mercedes Menchon <mer.menchon@gmail.com>
2023-10-02 16:57:12 -0300

    Modifique el login

--- unicen-visualizacion-master/TP2/EntregaFinal/js/login.js
+++ unicen-visualizacion-master/TP2/EntregaFinal/js/login.js
@@ -1,7 +1,7 @@
 "use stict";
 document.addEventListener("DOMContentLoaded", iniciarPagina);
 document.querySelector(".formRegistrarme").classList.add("ocultarFormulario");
-document.querySelector("#btn-loginIniciarSesion").classList.add(".click");
+document.querySelector("#btn-loginIniciarSesion").classList.add("click");
 function iniciarPagina(){
     document.querySelector(".btn_menu").addEventListener("click", mostrarNav);
    
@@ -18,20 +18,21 @@ function iniciarPagina(){
 
 
  /*LOGIN*/
- document.querySelector('#btn-loginIniciarSesion').addEventListener("click",MostrarFormLogin);
- document.querySelector('#btn-loginRegistrarme').addEventListener("click",MostrarFormLogin2);
-//HAYQ QUE AGREGAR QUE CAMBIE DE CLASE EL BOTON A CLICK
-   // document.querySelector("#btn-loginIniciarSesion").classList.toggle(".click");
-    //document.querySelector("#btn-loginRegistrarme").classList.toggle(".click");
- function MostrarFormLogin(){
+ document.querySelector('#btn-loginIniciarSesion').addEventListener("click",MostrarFormIniciarSesion);
+ document.querySelector('#btn-loginRegistrarme').addEventListener("click",MostrarFormRegistrarme);
+
+ function MostrarFormIniciarSesion(){
     
      document.querySelector(".formIniciarSesion").classList.remove("ocultarFormulario");
      document.querySelector(".formRegistrarme").classList.add("ocultarFormulario");
-   
+     CambiarBotonesLogin();
 }
-function MostrarFormLogin2(){
+function MostrarFormRegistrarme(){
          document.querySelector(".formIniciarSesion").classList.add("ocultarFormulario");
      document.querySelector(".formRegistrarme").classList.remove("ocultarFormulario");
-   
+     CambiarBotonesLogin();
 }
-
+ function CambiarBotonesLogin(){
+ document.querySelector("#btn-loginIniciarSesion").classList.toggle("click");
+document.querySelector("#btn-loginRegistrarme").classList.toggle("click");
+ }
