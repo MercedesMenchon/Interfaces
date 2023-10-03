55288e14ab721f1ab8d6346dd9a6bc4c6f11c693
Luciana Y. Romero <lu.yael.r@gmail.com>
2023-10-02 22:21:11 -0300

    cambios

--- unicen-visualizacion-master/TP2/EntregaFinal/js/main.js
+++ unicen-visualizacion-master/TP2/EntregaFinal/js/main.js
@@ -3,74 +3,74 @@
 
 document.addEventListener("DOMContentLoaded", iniciarPagina);
 
-function iniciarPagina(){
+function iniciarPagina() {
     document.querySelector(".btn_menu").addEventListener("click", mostrarNav);
-   
-    function mostrarNav(){
+
+    function mostrarNav() {
         document.querySelector(".navegacion").classList.toggle("show");
     }
 }
 
 
-/*CARDS AGREGAR FAVORITO*/ 
- 
- let cards = document.querySelectorAll(".card");
- let botonesCarritos =[];
- let botonesAddCarrito = document.querySelectorAll("#btn-addCarrito");
- let botonesDropCarrito = document.querySelectorAll("#btn-dropCarrito");
+/*CARDS AGREGAR FAVORITO*/
+
+let cards = document.querySelectorAll(".card");
+let botonesCarritos = [];
+let botonesAddCarrito = document.querySelectorAll("#btn-addCarrito");
+let botonesDropCarrito = document.querySelectorAll("#btn-dropCarrito");
 
 
 
 
 
- for(let i =0;i<cards.length;i++){
+for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("mouseover", hacerHoverfav);
- }
+}
 console.log(cards);
 
-function hacerHoverfav(){
-    
-    let botonlike =  this.querySelector("#btn-addLike");
-    botonlike.classList.toggle("likeShow"); 
-   this.querySelector("#btn-addLike").addEventListener("click",addLike);
- this.addEventListener("mouseout", hacerHoverfav);
+function hacerHoverfav() {
 
-function addLike(){
-    botonlike.classList.toggle("likeShowFijo");
+    let botonlike = this.querySelector("#btn-addLike");
+    botonlike.classList.toggle("likeShow");
+    this.querySelector("#btn-addLike").addEventListener("click", addLike);
+    this.addEventListener("mouseout", hacerHoverfav);
+
+    function addLike() {
+        botonlike.classList.toggle("likeShowFijo");
+    }
 }
-}
 
-/*CARDS AGREGAR AL CARRITO*/ 
+/*CARDS AGREGAR AL CARRITO*/
 
-let payCards =document.querySelectorAll(".payCard");
+let payCards = document.querySelectorAll(".payCard");
 
-for(let i=0; i< payCards.length; i++){
+for (let i = 0; i < payCards.length; i++) {
     payCards[i].addEventListener("mouseover", hacerHoverCarrito);
     payCards[i].addEventListener("mouseout", hacerHoverCarrito);
     payCards[i].querySelector("#btn-addCarrito").addEventListener("click", modificarCarrito);
     payCards[i].querySelector("#btn-dropCarrito").addEventListener("click", modificarCarrito);
- }
+}
 
-   function hacerHoverCarrito(){
-        this.querySelector("#btn-addCarrito").classList.toggle("carritoCompraShow");
+function hacerHoverCarrito() {
+    this.querySelector("#btn-addCarrito").classList.toggle("carritoCompraShow");
 
 }
 
-function modificarCarrito(){
+function modificarCarrito() {
     this.parentNode.querySelector("#btn-dropCarrito").classList.toggle("carritoCompraShow");
- }
+}
 
 
 /*CARDS FREE*/
-let freeCards =document.querySelectorAll(".freeCard");
+let freeCards = document.querySelectorAll(".freeCard");
 
-for(let i=0; i< freeCards.length; i++){
+for (let i = 0; i < freeCards.length; i++) {
     freeCards[i].addEventListener("mouseover", hacerHoverPlay);
     freeCards[i].addEventListener("mouseout", hacerHoverPlay);
 
 }
 
-function hacerHoverPlay(){
-   this.querySelector("#btn-play").classList.toggle("play");
- }
+function hacerHoverPlay() {
+    this.querySelector("#btn-play").classList.toggle("play");
+}
 
