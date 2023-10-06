dfe98a5e025ee01e46421e714aef365bb1906b0b
Mercedes Menchon <mer.menchon@gmail.com>
2023-10-06 19:56:40 -0300

    avance en la pagina de 4lineas

--- unicen-visualizacion-master/TP2/EntregaFinal/js/4enLinea.js
+++ unicen-visualizacion-master/TP2/EntregaFinal/js/4enLinea.js
@@ -1,4 +1,7 @@
 "use strict";
+
+
+
 let cantVotosInicial=8250;
 document.querySelector(".cant-votos").innerHTML= cantVotosInicial+" votos";
 
@@ -8,3 +11,54 @@ function addVoto(){
   document.querySelector(".cant-votos").innerHTML= cantVotosInicial+" votos";
 }
 
+
+
+let comentariosViejos=[{
+  "usuario":"Mauro",
+  "comentario": "que buen juego!"
+},
+{
+  "usuario":"Mauro",
+  "comentario": "que buen juego!"
+},
+{
+  "usuario":"Mauro",
+  "comentario": "que buen juego!"
+},
+{
+  "usuario":"Mauro",
+  "comentario": "que buen juego!"
+},
+{
+  "usuario":"Mauro",
+  "comentario": "que buen juego!"
+},
+];
+function showComentarios(){
+  let lista = "";
+  for(let i=0; i<comentariosViejos.length;i++) {
+lista += "<h3>" + comentariosViejos[i].usuario + "</h3> <p>" + comentariosViejos[i].comentario + "</p>"
+  }
+  
+document.querySelector(".listaComentarios").innerHTML = lista;
+
+
+}
+showComentarios();
+
+
+
+document.querySelector("#btn-comentar").addEventListener("click",addComentario);
+
+function addComentario(){
+  if(!document.querySelector("#comentario").empty){
+  let comentario = document.querySelector("#comentario").value;
+  let nuevoComentario={
+    "usuario":"Laura",
+    "comentario":comentario
+  }
+  comentariosViejos.push(nuevoComentario);
+   document.querySelector("#comentario").value="";
+  showComentarios();
+}
+}
