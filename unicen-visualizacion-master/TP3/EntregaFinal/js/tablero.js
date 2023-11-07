3eda977f44eef5008a214d4d7f18f4b375f30656
Luciana Y. Romero <lu.yael.r@gmail.com>
2023-11-07 17:48:22 -0300

    emparejando

--- unicen-visualizacion-master/TP3/EntregaFinal/js/tablero.js
+++ unicen-visualizacion-master/TP3/EntregaFinal/js/tablero.js
@@ -1,17 +1,16 @@
 "Use strict";
 //CREAMOS LA CLASE TABLERO
 class Tablero {
-
-
   constructor(filas, columnas, canvas, ctx) {
     this.columnas = columnas;
     this.filas = filas;
     this.matriz = [];
-this.armarMatriz();
+    this.armarMatriz();
     this.canvas = canvas;
     this.ctx = ctx;
-    this.cantFichas= this.getCantFichas();
+    this.cantFichas = this.getCantFichas();
   }
+
 
   getFilas() {
     return this.filas;
@@ -25,10 +24,10 @@ this.armarMatriz();
     return (this.canvas.width - this.getColumnas() * this.getAnchoCelda()) / 2;
   }
 
-  getEspacioBlancoY() { 
-    return (this.canvas.height - this.getFilas() * this.getAltoCelda()) * 4 / 5; 
+  getEspacioBlancoY() {
+    return (this.canvas.height - this.getFilas() * this.getAltoCelda()) * 4 / 5;
   }
-  
+
 
   getAnchoCelda() {
     return this.canvas.width * .7 / this.columnas;
@@ -37,33 +36,35 @@ this.armarMatriz();
   getAltoCelda() {
     return this.canvas.height * .7 / this.filas;
   }
-getFilas(){
-  return this.filas;
-}
+  getFilas() {
+    return this.filas;
+  }
 
-getColumnas(){
-  return this.columnas;
-}
+  getColumnas() {
+    return this.columnas;
+  }
   getRadioFicha() {
     return this.getAltoCelda() / 2 * 0.8;
   }
 
-  getCantFichas(){
-  return this.getFilas() * this.getColumnas();
-}
-  armarMatriz(x,y) {
-    for (let fila = 0; fila < this. getFilas(); fila++) {
-      this.matriz[fila] = [];
-      for (let columna = 0; columna < this.getColumnas(); columna++) {
-         this.matriz[fila][columna]=0;
-  
-      }
-    }
-
+  getCantFichas() {
+    return this.getFilas() * this.getColumnas();
   }
 
+  armarMatriz(x, y) {
+     for (let fila = 0; fila < this.getFilas(); fila++) {
+       this.matriz[fila] = [];
+       for (let columna = 0; columna < this.getColumnas(); columna++) {
+         this.matriz[fila][columna] = 0;
+       }
+       
+     }
+ 
+   }
+ 
+
   armarTablero() {
-    
+
     //PARA BORRAR TODO LO QUE YA ESTA, DEBEMOS UTILIZAR:
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     //this.ctx.lineWidth = 3;
@@ -74,32 +75,32 @@ getColumnas(){
         // Calcula el espacio en blanco a la izquierda y arriba para centrar el tablero
         const x = this.getEspacioBlancoX() + columna * this.getAnchoCelda();
         const y = this.getEspacioBlancoY() + fila * this.getAltoCelda();
-        const casillero = new Casillero(x,y, this.getAnchoCelda(), this.getAltoCelda(), this.getRadioFicha());
-       this.matriz[fila][columna]=casillero;
-       casillero.dibujar(this.ctx);
+        const casillero = new Casillero(x, y, this.getAnchoCelda(), this.getAltoCelda(), this.getRadioFicha());
+        this.matriz[fila][columna] = casillero;
+        casillero.dibujar(this.ctx);
 
         //this.ctx.fillStyle = "rgb(0,85,164)";
-      //  this.ctx.fillRect(x, y, this.getAnchoCelda(), this.getAltoCelda());
-      //estos son los bordes rosas: 
-      this.ctx.strokeStyle = "rgb(255,0,255)";
+        //  this.ctx.fillRect(x, y, this.getAnchoCelda(), this.getAltoCelda());
+        //estos son los bordes rosas: 
+        this.ctx.strokeStyle = "rgb(255,0,255)";
         this.ctx.strokeRect(x, y, this.getAnchoCelda(), this.getAltoCelda());
         // Dibujamos las fichas según el estado de la matriz
- /*       if (this.matriz[fila][columna] === 0) {
-          
+        /*       if (this.matriz[fila][columna] === 0) {
+                 
+       
+               }
+               else {
+       
+                 const ficha = this.matriz[fila][columna];
+                 ficha.dibujarFicha(this.ctx, x + this.getAnchoCelda() / 2, y + this.getAltoCelda() / 2);
+                 this.ctx.strokeRect(x, y, this.getAnchoCelda(), this.getAltoCelda());
+                 this.ctx.fillStyle = "rgb(255,0,255)";
+         
+               }*/
 
-        }
-        else {
-
-          const ficha = this.matriz[fila][columna];
-          ficha.dibujarFicha(this.ctx, x + this.getAnchoCelda() / 2, y + this.getAltoCelda() / 2);
-          this.ctx.strokeRect(x, y, this.getAnchoCelda(), this.getAltoCelda());
-          this.ctx.fillStyle = "rgb(255,0,255)";
-  
-        }*/
-        
       }
     }
-    
+
   }
 
 /*
