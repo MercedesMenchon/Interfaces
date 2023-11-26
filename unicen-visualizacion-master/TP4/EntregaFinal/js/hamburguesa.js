b9a9ec2e2cc3b25ff2e1fff4e046f7191e524a16
Luciana Y. Romero <lu.yael.r@gmail.com>
2023-11-26 15:52:14 -0300

    menu y cruz

--- unicen-visualizacion-master/TP4/EntregaFinal/js/hamburguesa.js
+++ unicen-visualizacion-master/TP4/EntregaFinal/js/hamburguesa.js
@@ -1,10 +1,31 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     var hamburguesa = document.querySelector('.hamburguesa');
+    var menuItems = document.querySelectorAll('.menu');
 
     hamburguesa.addEventListener('click', function () {
         this.classList.toggle('abierto');
-        console.log("cambio");
+
+        // Oculta todos los elementos
+        menuItems.forEach(function (elemento) {
+            elemento.classList.remove('desplegado');
+        });
+
+        // Muestra el primer elemento
+        if (this.classList.contains('abierto')) {
+            menuItems[0].classList.add('desplegado');
+            var index = 1;
+
+            // Función para mostrar los elementos de a uno con un intervalo de tiempo
+            function mostrarSiguiente() {
+                if (index < menuItems.length) {
+                    menuItems[index].classList.add('desplegado');
+                    index++;
+                    setTimeout(mostrarSiguiente, 400); // 400 ms de intervalo entre elementos
+                }
+            }
+
+            // Inicia la función de mostrarSiguiente después de un breve tiempo
+            setTimeout(mostrarSiguiente, 400);
+        }
     });
 });
