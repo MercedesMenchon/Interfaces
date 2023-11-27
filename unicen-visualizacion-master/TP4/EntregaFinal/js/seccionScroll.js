49686cc9a028b5af9919128508866f79a3736982
Mercedes Menchon <mer.menchon@gmail.com>
2023-11-27 16:21:23 -0300

    nav

--- unicen-visualizacion-master/TP4/EntregaFinal/js/seccionScroll.js
+++ unicen-visualizacion-master/TP4/EntregaFinal/js/seccionScroll.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 "use strict";
 document.addEventListener('DOMContentLoaded', function () {
     var opciones = document.querySelectorAll('.opcion');
@@ -17,5 +18,25 @@ document.addEventListener('DOMContentLoaded', function () {
         
         }
       });
+=======
+"use strict"
+document.addEventListener('DOMContentLoaded', function () {
+    var opciones = document.querySelectorAll('.opcion');
+    var ventana = document.querySelector('.ventana');
+
+    ventana.addEventListener('scroll', function () {
+        opciones.forEach(function (opcion) {
+            var columnaIzquierda = opcion.querySelector('.columnaIzquierda');
+            var columnaDerecha = opcion.querySelector('.columnaDerecha');
+
+            var rect = opcion.getBoundingClientRect();
+
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                columnaIzquierda.classList.remove('oculta');
+            } else {
+                columnaIzquierda.classList.add('oculta');
+            }
+        });
+>>>>>>> Stashed changes
     });
   });
