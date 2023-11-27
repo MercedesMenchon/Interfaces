d5693f31059b4432f8e92cba2fa21f3fd650bff1
Luciana Y. Romero <lu.yael.r@gmail.com>
2023-11-27 19:06:11 -0300

    loading

---
+++ unicen-visualizacion-master/TP4/EntregaFinal/js/loading.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var barraLoading = document.querySelector(".barraLoading");
+    var arania = document.querySelector(".arania");
+    var porcentaje = document.querySelector(".porcentaje");
+
+    setTimeout(function () {
+        barraLoading.style.width = "100%";
+        arania.style.left = (800 - barraLoading.offsetWidth + arania.offsetWidth) + "px";
+        porcentaje.innerText = "100%";
+    
+    }, 1000); // Inicia la carga después de 1 segundo
+
+    setTimeout(function () {
+        var porcentajeValue = 0;
+        var interval = setInterval(function () {
+            porcentajeValue += 1;
+            porcentaje.innerText = porcentajeValue + "%";
+
+            if (porcentajeValue >= 100) {
+                clearInterval(interval);
+                window.location.href = 'index.html';
+            }
+            
+        }, 80); // Ajusta la velocidad de aumento del porcentaje
+        
+    }, 1000); // Inicia la carga después de 1 segundo
+});
