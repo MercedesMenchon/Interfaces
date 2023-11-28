1dd9cf759eef31dd2a4c41a06be6b81e00ea0a04
Luciana Y. Romero <lu.yael.r@gmail.com>
2023-11-28 20:06:02 -0300

    fusion de trabajos

---
+++ unicen-visualizacion-master/TP4/EntregaFinal/js/seccionScroll.js
@@ -0,0 +1,41 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+    var opciones = document.querySelectorAll('.opcion');
+    var ventana = document.querySelector('.ventana');
+  
+    ventana.addEventListener('scroll', function () {
+      opciones.forEach(function (opcion) {
+        var columnaIzquierda = opcion.querySelector('.columnaIzquierda');
+       
+        var rect = opcion.getBoundingClientRect();
+  
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+          columnaIzquierda.classList.remove('transparencia');
+       
+        } else {
+          columnaIzquierda.classList.add('transparencia');
+          "use strict";
+          document.addEventListener('DOMContentLoaded', function () {
+              var opciones = document.querySelectorAll('.opcion');
+              var ventana = document.querySelector('.ventana');
+          
+              ventana.addEventListener('scroll', function () {
+                opciones.forEach(function (opcion) {
+                  var columnaIzquierda = opcion.querySelector('.columnaIzquierda');
+          
+                  var rect = opcion.getBoundingClientRect();
+          
+                  if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                    columnaIzquierda.classList.remove('transparencia');
+          
+                  } else {
+                    columnaIzquierda.classList.add('transparencia');
+          
+                  }
+                });
+              });
+            });
+        }
+      });
+    });
+  });
