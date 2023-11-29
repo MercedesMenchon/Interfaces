f6433432cb08b7fb12b10159e439dfed3246e2c9
Mercedes Menchon <mer.menchon@gmail.com>
2023-11-28 23:50:44 -0300

    modificacion

--- unicen-visualizacion-master/TP4/EntregaFinal/js/cardsPersonajes.js
+++ unicen-visualizacion-master/TP4/EntregaFinal/js/cardsPersonajes.js
@@ -1,21 +1,25 @@
+"use strict"
 function handleScroll() {
   const personajesSection = document.querySelector('.personajes');
   const columnasPersonajes = document.querySelectorAll('.columnaPersonajes');
 
   const sectionTop = personajesSection.getBoundingClientRect().top;
+  const sectionBottom = personajesSection.getBoundingClientRect().bottom;
 
-  if (sectionTop < window.innerHeight * 0.75) {
+  // Verifica si la sección está al menos parcialmente visible en la ventana
+  if (sectionTop < window.innerHeight && sectionBottom > 0) {
       columnasPersonajes.forEach((columna, index) => {
           setTimeout(() => {
               columna.classList.add('visible');
           }, index * 100); // Añade un pequeño retraso entre cada columna para un efecto secuencial
       });
-      window.removeEventListener('scroll', handleScroll); // Desactiva el evento después de animar las columnas
+  } else {
+      // Si la sección no está visible, quita la clase 'visible' para permitir futuras animaciones
+      columnasPersonajes.forEach(columna => {
+          columna.classList.remove('visible');
+      });
   }
 }
 
 // Agregar el evento de desplazamiento
 window.addEventListener('scroll', handleScroll);
-
-// Llamar a la función una vez para verificar al cargar la página
-handleScroll();
